d7775efc560f52ce9b2deefc97615c8b560a3bf9
Oskar Wahlberg <oskar.wahlberg99@outlook.com>
2017-10-16 09:55:31 +0200

    Continued polishing on the image slider indicator images

--- assets/js/index-image-slider.js
+++ assets/js/index-image-slider.js
@@ -2,6 +2,7 @@
 
 var img;
 var img_amount;
+var img_indicators;
 var displayed_img = 0;
 var opacity = 1;
 
@@ -9,6 +10,7 @@ var opacity = 1;
 function GetImages() {
     img = document.getElementsByClassName("i_is");
     img_amount = img.length;
+    img_indicators = document.getElementsByClassName("i_id");
     
     HideImages();
 }
@@ -19,12 +21,15 @@ function HideImages() {
     for (i = 1; i < img_amount; i++) {
         img[i].style.display = "none";
     }
+    
+    img_indicators[displayed_img].style.opacity = ".5";
 }
 
 // Make the arrows in the imageslider work
 function ImageSwitch(x) {
     if (x === 1) {
         img[displayed_img].style.display = "none";
+        img_indicators[displayed_img].style.opacity = "1";
         
         if(displayed_img === img_amount - 1) {
             displayed_img = 0;
@@ -33,8 +38,10 @@ function ImageSwitch(x) {
         }
         
         img[displayed_img].style.display = "block";
+        img_indicators[displayed_img].style.opacity = ".5";
     } else if (x === -1) {
         img[displayed_img].style.display = "none";
+        img_indicators[displayed_img].style.opacity = "1";
         
         if(displayed_img === 0) {
             displayed_img = img_amount - 1;
@@ -43,6 +50,7 @@ function ImageSwitch(x) {
         }
         
         img[displayed_img].style.display = "block";
+        img_indicators[displayed_img].style.opacity =".5";
     }
 }
 
@@ -50,8 +58,12 @@ function ImageSwitch(x) {
 
 function ImageIndicatorSwitch(x) {
     img[displayed_img].style.display = "none";
+    img_indicators[displayed_img].style.opacity = "1";
+    
     displayed_img = x;
+    
     img[displayed_img].style.display = "block";
+    img_indicators[displayed_img].style.opacity = ".5";
 }
 
 // IMAGE SLIDER INDICATION END
